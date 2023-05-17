ad51f41dcf0efec71dd7fb773418d0f8b8cd5ba8
fahad-zakir <fahadzakir81@gmail.com>
2023-05-16 20:00:24 -0500

    updated-route

--- src/Controller/JsonController.php
+++ src/Controller/JsonController.php
@@ -5,13 +5,9 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 
 class JsonController extends AbstractController
 {
-    /**
-     * @Route("/json", name="json")
-     */
     public function index(): Response
     {
     $file = '/Applications/MAMP/htdocs/test-symfony/data.json';
